b615d039af8028523ef911109016f5bb3baebf2e
TAKATANI Hideaki <ev18d001@steng.u-hyogo.ac.jp>
2019-05-29 21:10:29 +0900

    examplesのスクリプトからpsoモジュールを読み込めるように改良
    pso.pyのシンボリックリンクを置くかわりに、exスクリプトにsys.pathへpsoのパスを追
    加する方法を採用した。

--- examples/ex1.py
+++ examples/ex1.py
@@ -1,5 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# このスクリプトが置かれたディレクトリの上位ディレクトリをモジュール検索パスに
+# 追加する。すなわち、psoモジュールを読み込めるようにする
+import sys
+import pathlib
+sys.path.append(pathlib.Path(__file__).absolute().parent.parent.as_posix())
 import pso
 
 if __name__ == "__main__":
